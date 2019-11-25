d7c077451b9dc9373ac614622c5909089396f47b
zhangq <zhangqiang014@chinasofti.com>
2019-11-25 17:58:11 +0800

    boot代码提交

---
+++ boot_start/src/main/java/com/loco/cn/boot_start/BootStartApplication.java
@@ -0,0 +1,35 @@
+package com.loco.cn.boot_start;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Arrays;
+
+@SpringBootApplication
+@EnableCaching
+public class BootStartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootStartApplication.class, args);
+    }
+
+
+    @Bean
+    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+        return args -> {
+
+            System.out.println("Let's inspect the beans provided by Spring Boot:");
+
+            String[] beanNames = ctx.getBeanDefinitionNames();
+            Arrays.sort(beanNames);
+            for (String beanName : beanNames) {
+                System.out.println(beanName);
+            }
+
+        };
+    }
+}
---
+++ boot_start/src/test/java/com/loco/cn/boot_start/BootStartApplicationTests.java
@@ -0,0 +1,41 @@
+package com.loco.cn.boot_start;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.net.URL;
+
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.junit.Assert.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = BootStartApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class BootStartApplicationTests {
+
+    @LocalServerPort
+    private int port;
+
+    private URL base;
+
+    @Autowired
+    private TestRestTemplate template;
+
+    @Before
+    public void setUp() throws Exception {
+        this.base = new URL("http://localhost:" + port + "/");
+    }
+
+    @Test
+    public void getHello() throws Exception {
+        ResponseEntity<String> response = template.getForEntity(base.toString(), String.class);
+        assertThat(response.getBody(), equalTo("Greetings from Spring Boot!"));
+    }
+
+}
